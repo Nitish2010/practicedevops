9eb9ee48b06a5886ab3294aed7d5f8fe5e975bf5
root <root@ip-172-31-80-191.ec2.internal>
2022-01-20 16:22:21 +0000

    made changes in feature-1

--- hello.java
+++ hello.java
@@ -2,9 +2,4 @@ class Hello
 {
 	public static void main(String args[])
 	{
-		System.out.println("hello");
-	}
-	
-made some changes in local machine i thought we have to clear some basic command 
-over the all local machines
-}
+		System.out.println("hello");}
